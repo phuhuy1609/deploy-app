365c97f2ff911c763b168303112b20be01a55985
huybean <huybean160999@gmail.com>
2022-12-15 11:20:15 +0700

    first upload

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 require('dotenv').config()
 app.get("/", (req, res) => {
-    res.send("hello world!")
+    res.send("hello world!-"+ process.env.nameHost)
 });
 
 app.listen("5000", () => {
