a8b3cce7236628eb4aeebd36c0a5111232454937
huybean <huybean160999@gmail.com>
2022-12-15 11:19:15 +0700

    first upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+require('dotenv').config()
+app.get("/", (req, res) => {
+    res.send("hello world!")
+});
+
+app.listen("5000", () => {
+    console.log("app listening port 5000")
+})
